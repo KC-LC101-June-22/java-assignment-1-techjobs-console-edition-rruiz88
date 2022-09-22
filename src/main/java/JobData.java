15c73a36259eda47a095b0a66758e7cce02a48a6
rruiz88 <roman.ruiz816@gmail.com>
2022-09-22 10:52:02 -0500

    attempt to lowercase search term results

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -77,9 +77,9 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -99,7 +99,19 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+
+            String aValue = row.get(value);
+
+            if (aValue.contains(value.toLowerCase())) {
+                jobs.add(row);
+            }
+        }
+        //findByValue(value);
+        return jobs;
+
     }
 
     /**
