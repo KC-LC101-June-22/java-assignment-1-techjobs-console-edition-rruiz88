a9d36945436f1dc94bc78f2711e6599af1b3593c
rruiz88 <roman.ruiz816@gmail.com>
2022-09-24 15:10:48 -0500

    Update JobData.java

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -102,16 +99,14 @@ public class JobData {
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
 
         for (HashMap<String, String> row : allJobs) {
+            for (Map.Entry<String, String> jobInfo : row.entrySet()) {
+                if (jobInfo.getValue().toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(row);
 
-            String aValue = row.get(value);
-
-            if (aValue.contains(value.toLowerCase())) {
-                jobs.add(row);
+                }
             }
         }
-        //findByValue(value);
         return jobs;
-
     }
 
     /**
